b2e1022831d45415e4a74063fa826c2dbef027d1
Wolfgang <Wolfgang@DELTAFLYER>
2017-06-07 10:21:04 +0200

    Add project files.

---
+++ BackbendWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Backbend {
+    /// <summary>
+    /// Interaction logic for BackbendWindow.xaml
+    /// </summary>
+    public partial class BackbendWindow {
+        public BackbendWindow() {
+            InitializeComponent();
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
---
+++ Test/DummyTest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Backbend.Test {
+    [TestClass]
+    public class DummyTest {
+        [TestMethod]
+        public void CanRunUnitTest() {
+        }
+    }
+}
