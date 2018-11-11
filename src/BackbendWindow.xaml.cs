89acf837d2b205142bd511cd4f1e5f919bd97970
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-11-11 12:06:18 +0100

    Add refresh button

--- src/BackbendWindow.xaml.cs
+++ src/BackbendWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Navigation;
@@ -11,6 +13,7 @@ using Aspenlaub.Net.GitHub.CSharp.Dvin.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Dvin.Repositories;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Helpers;
+using TimeSpan = System.TimeSpan;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Backbend {
     /// <summary>
@@ -35,45 +38,46 @@ namespace Aspenlaub.Net.GitHub.CSharp.Backbend {
         }
 
         private async void BackbendWindow_OnLoaded(object sender, RoutedEventArgs e) {
-            NavigateToMessage($"Starting {Constants.BackbendAppId}&hellip;");
+            await NavigateToMessage($"Starting {Constants.BackbendAppId}&hellip;");
 
             var dvinApp = await vDvinRepository.LoadAsync(Constants.BackbendAppId);
             if (dvinApp == null) {
-                NavigateToMessage($"{Constants.BackbendAppId} app not found");
+                await NavigateToMessage($"{Constants.BackbendAppId} app not found");
                 return;
             }
 
             Wait.Until(() => dvinApp.IsPortListenedTo(), TimeSpan.FromSeconds(5));
-            if (!dvinApp.IsPortListenedTo() && !StartAppAndReturnSuccess(dvinApp)) {
+            if (!dvinApp.IsPortListenedTo() && !await StartAppAndReturnSuccess(dvinApp)) {
                 return;
             }
 
             Cursor = Cursors.Wait;
-            Width = 720;
-            Height = 720;
+            Width = 660;
+            Height = 660;
+            vNavigated = false;
             HtmlOutput.Navigate("http://localhost:" + dvinApp.Port);
         }
 
-        private bool StartAppAndReturnSuccess(IDvinApp dvinApp) {
+        private async Task<bool> StartAppAndReturnSuccess(IDvinApp dvinApp) {
             var fileSystemService = new FileSystemService();
             var errorsAndInfos = new ErrorsAndInfos();
             if (!dvinApp.HasAppBeenPublishedAfterLatestSourceChanges(Environment.MachineName, fileSystemService)) {
                 dvinApp.Publish(fileSystemService, true, errorsAndInfos);
                 if (errorsAndInfos.AnyErrors()) {
-                    NavigateToMessage(string.Join("<br>", errorsAndInfos.Errors));
+                    await NavigateToMessage(string.Join("<br>", errorsAndInfos.Errors));
                     return false;
                 }
             }
 
             if (!dvinApp.HasAppBeenPublishedAfterLatestSourceChanges(Environment.MachineName, fileSystemService)) {
-                NavigateToMessage($"{Constants.BackbendAppId} has not been published since the latest source code changes");
+                await NavigateToMessage($"{Constants.BackbendAppId} has not been published since the latest source code changes");
                 return false;
             }
 
             vProcess = dvinApp.Start(fileSystemService, errorsAndInfos);
             Wait.Until(() => dvinApp.IsPortListenedTo(), TimeSpan.FromSeconds(5));
             if (errorsAndInfos.AnyErrors()) {
-                NavigateToMessage(string.Join("<br>", errorsAndInfos.Errors));
+                await NavigateToMessage(string.Join("<br>", errorsAndInfos.Errors));
                 return false;
             }
 
@@ -81,7 +85,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.Backbend {
                 return true;
             }
 
-            NavigateToMessage($"{Constants.BackbendAppId} started but not listening");
+            await NavigateToMessage($"{Constants.BackbendAppId} started but not listening");
             return false;
         }
 
@@ -93,10 +97,11 @@ namespace Aspenlaub.Net.GitHub.CSharp.Backbend {
             }
         }
 
-        private void NavigateToMessage(string message) {
+        private async Task NavigateToMessage(string message) {
+            vNavigated = false;
             var markup = "<html><head></head><body><p>" + message + "</p></body></html>";
             HtmlOutput.NavigateToString(markup);
-            Wait.Until(() => vNavigated, TimeSpan.FromSeconds(5));
+            await Task.Run(() => Wait.Until(() => vNavigated, TimeSpan.FromSeconds(5)));
         }
     }
 }
---
+++ src/Web/wwwroot/lib/js/backbend.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    FoldersLoaded();
+});
+
+function FoldersLoaded() {
+    $("#refresh").click(function () {
+        location.reload();
+    });
+}
